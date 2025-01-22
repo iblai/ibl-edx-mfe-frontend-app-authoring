fbcb3a74c79374ed28f2e8dd393c1963227611e1
Sone Gillis <sonegillis@gmail.com>
2025-01-22 19:20:31 +0100

    Replace config.defaultValue with config.formKey in course-metadata-section

--- src/schedule-and-details/course-metadata-section/index.jsx
+++ src/schedule-and-details/course-metadata-section/index.jsx
@@ -8,7 +8,7 @@ function renderField(courseSettings, param, onChange) {
     return (
       <Dropdown className="bg-white">
         <Dropdown.Toggle variant="outline-primary" id={param.fieldId}>
-          {courseSettings[param.config.formKey]}
+          {param.config.label}
         </Dropdown.Toggle>
         <Dropdown.Menu>
           {courseSettings[param.config.optionsKey]?.map((option) => (
